2f4f387ee18b7fd66bce03773b9caaa0c3a613e7
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-04-05 00:25:07 -0300

    Implements immutable-hash API.
    except it still lacks filter/diff.

--- benchmarks/immutable-hash.js
+++ benchmarks/immutable-hash.js
@@ -108,58 +108,58 @@ benchmark("Calling patch() on hash(100000)", function() {
 })
 
 
-// benchmark("ImmutableHash integration()", function () {
-//     var hash = ImmutableHash()
-//
-//     var hash2 = hash.patch({ foo: "bar" })
-//
-//     var hash3 = hash2.patch({ foo: "baz" })
-//
-//     var hash4 = hash3.patch({ "foo":  null })
-//
-//     var hash5 = hash4.patch({ bar: { baz: true } })
-//
-//     var hash6 = hash5.patch({ bar: { fuux: false } })
-//
-//     var hash7 = hash6.patch({ bar : { baz: "hello world" }})
-//
-//     var hash8 = hash7.map("bar", function (x) {
-//         return String(x)
-//     })
-//
-//     var hash9 = hash8.patch({ baz: { one: "hello", two: "world" } })
-//
-//     var hash10 = hash9.map(function (x) {
-//         return x.patch("prop", { live: 42 })
-//     })
-// })
-//
-// benchmark("diffpatcher integration()", function() {
-//     var hash = {}
-//
-//     var hash2 = patch(hash, { foo: "bar" })
-//
-//     var hash3 = patch(hash2, { "foo": "baz" })
-//
-//     var hash4 = patch(hash3, { "foo": null })
-//
-//     var hash5 = patch(hash4, { bar: { baz: true } })
-//
-//     var hash6 = patch(hash5, { bar: { fuux: false } })
-//
-//     var hash7 = patch(hash6, { bar: { baz: "hello world" } })
-//
-//     var hash8 = patch(hash7, {
-//         bar: Object.keys(hash7.bar).reduce(function (acc, k) {
-//             acc[k] = String(hash7.bar[k])
-//             return acc
-//         }, {})
-//     })
-//
-//     var hash9 = patch(hash8, { baz: { one: "hello", two: "world" } })
-//
-//     var hash10 = Object.keys(hash9).reduce(function (acc, k) {
-//         acc[k] = patch(hash9[k], { prop: { live: 42 } })
-//         return acc
-//     }, {})
-// })
+benchmark("ImmutableHash integration()", function () {
+    var hash = ImmutableHash()
+
+    var hash2 = hash.patch({ foo: "bar" })
+
+    var hash3 = hash2.patch({ foo: "baz" })
+
+    var hash4 = hash3.patch({ "foo":  null })
+
+    var hash5 = hash4.patch({ bar: { baz: true } })
+
+    var hash6 = hash5.patch({ bar: { fuux: false } })
+
+    var hash7 = hash6.patch({ bar : { baz: "hello world" }})
+
+    var hash8 = hash7.map("bar", function (x) {
+        return String(x)
+    })
+
+    var hash9 = hash8.patch({ baz: { one: "hello", two: "world" } })
+
+    var hash10 = hash9.map(function (x) {
+        return x.patch("prop", { live: 42 })
+    })
+})
+
+benchmark("diffpatcher integration()", function() {
+    var hash = {}
+
+    var hash2 = patch(hash, { foo: "bar" })
+
+    var hash3 = patch(hash2, { "foo": "baz" })
+
+    var hash4 = patch(hash3, { "foo": null })
+
+    var hash5 = patch(hash4, { bar: { baz: true } })
+
+    var hash6 = patch(hash5, { bar: { fuux: false } })
+
+    var hash7 = patch(hash6, { bar: { baz: "hello world" } })
+
+    var hash8 = patch(hash7, {
+        bar: Object.keys(hash7.bar).reduce(function (acc, k) {
+            acc[k] = String(hash7.bar[k])
+            return acc
+        }, {})
+    })
+
+    var hash9 = patch(hash8, { baz: { one: "hello", two: "world" } })
+
+    var hash10 = Object.keys(hash9).reduce(function (acc, k) {
+        acc[k] = patch(hash9[k], { prop: { live: 42 } })
+        return acc
+    }, {})
+})
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// :: a -> String
+var classOf = Function.call.bind({}.toString)
+
 // :: a -> Bool
 function isObject(a){ return Object(a) === a }
+
+// :: a -> Bool
+function isString(a){ return classOf(a) === '[object String]' }
+
+// :: a -> Bool
+function isFunction(a){ return typeof a == 'function' }
 
 // :: Object*, Object -> Object*
 function extend(target, source) {
@@ -8,12 +17,25 @@ function extend(target, source) {
     var key = keys[i]
     if (isObject(source[key])) {
       if (!target[key]) target[key] = source[key]
-      else              extend(target[key], source[key])
+      else              target[key] = extend(clone(target[key]), source[key])
     }
     else if (target[key] !== source[key])
       target[key] = source[key]
   }
   return target
+}
+
+// :: [String], a -> { String -> a }
+// :: String, a -> { String -> a }
+function toObject(parts, value) {
+  if (isObject(parts))  return parts
+  if (isString(parts))  parts = parts.split('.')
+  var len = parts.length - 1, result = {}
+  parts.reduce(function(result, part, i) {
+    return i == len?  result[part] = value
+    :                 result[part] = {}
+  }, result)
+  return result
 }
 
 // :: Object -> Object
@@ -24,24 +46,33 @@ var protoOf = Object.getPrototypeOf
 
 // :: type ImmutableHashT a
 var ImmutableHashT = {
-  // :: @ImmutableHashT => Object -> ImmutableHashT <| b
-  patch: function(struct) {
+  // :: @ImmutableHash => Object -> ImmutableHash <| Object
+  // :: @ImmutableHash => String, a -> ImmutableHash <| Object
+  // :: @ImmutableHash => [String], a -> ImmutableHash <| Object
+  patch: function(struct, value) {
     var result = new ImHash()
-    result._data = extend(clone(this._data), struct)
+    result._data = extend(clone(this._data), toObject(struct, value))
     return result
   }
 
-  // :: String -> a
+  // :: @ImmutableHash => String -> a
 , get: function(key) {
-    return this._data[key]
+    return key.split('.').reduce(function(result, key) {
+      return result[key]
+    }, this._data)
   }
 
-  // :: String -> Bool
+  // :: @ImmutableHash => String -> Bool
 , has: function(key) {
-    return key in this._data
+    var d = this._data
+    return key.split('.').every(function(key) {
+      var r = key in d
+      d = d[key]
+      return r
+    })
   }
 
-  // :: () -> Object
+  // :: @ImmutableHash => () -> Object
 , toJSON: function() {
     var result = {}
     for (var key in this._data) {
@@ -49,9 +80,25 @@ var ImmutableHashT = {
     }
     return result
   }
+
+  // :: @ImmutableHash => (a -> b) -> ImmutableHash
+  // :: @ImmutableHash => String, (a -> b) -> ImmutableHash
+  // :: @ImmutableHash => [String], (a -> b) -> ImmutableHash
+, map: function(query, lambda) {
+    if (isFunction(query)) lambda = query, query = ''
+
+    var data = query? this.get(query) : this._data
+    var diff = {}
+
+    for (var key in data) {
+      diff[key] = lambda(data[key], key)
+    }
+
+    return query?  this.patch(query, diff) : this.patch(diff)
+  }
 }
 
-// Object ImHash
+// type ImmutableHash <| ImmutableHashT
 ImHash.prototype = ImmutableHashT
 function ImHash() {
   this._data = clone(null)
